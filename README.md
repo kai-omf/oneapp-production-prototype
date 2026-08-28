15420fc7a9d960175a14b264ca9aae89e57ce414
kai-omf <kai.jei@omf.com>
2026-08-28 15:57:44 -0400

    Publish OneApp Production prototype

---
+++ README.md
@@ -0,0 +1,3 @@
+# OneApp Production Prototype
+
+This repository contains the generated public prototype. The source implementation is maintained privately in Singularity.
---
+++ _next/static/chunks/app/layout-32bb016a150069c9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6361:()=>{},7340:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6361,23))}},e=>{e.O(0,[820,587,968,358],()=>e(e.s=7340)),_N_E=e.O()}]);
